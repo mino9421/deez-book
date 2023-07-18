0850a0233cb831010ada0e5f03ae7d336ff708da
Mino <mino9421@gmail.com>
2023-07-18 15:01:55 -0400

    testing some modifications for deployment test

--- src/server/helpers/ssgHelper.ts
+++ src/server/helpers/ssgHelper.ts
@@ -1,11 +1,11 @@
 import { appRouter } from "~/server/api/root";
-// import { prisma } from "~/server/db";
 import { createServerSideHelpers } from '@trpc/react-query/server';
-import { createContext } from '~/server/context';
+// import { createContext } from '~/server/context';
 import superjson from 'superjson';
+import { prisma } from "~/server/db";
 
 const helpers = createServerSideHelpers({
   router: appRouter,
-  ctx: await createContext(),
+  ctx: { prisma, userId: null },
   transformer: superjson, // optional - adds superjson serialization
 });
